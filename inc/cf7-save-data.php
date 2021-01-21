edbc4061ea0c482eb99af736a0335dc8aa7a0030
Gabe Herbert <gh@gabeherbert.com>
2021-01-20 16:12:55 -0800

    fixes plugin; no improvement

--- inc/cf7-save-data.php
+++ inc/cf7-save-data.php
@@ -1,34 +1,34 @@
 <?php
 
-add_action('wpcf7_before_send_mail', 'CF7EC_save_form' );
+add_action( 'wpcf7_before_send_mail', 'CF7EC_save_form' );
 
 function CF7EC_save_form( $wpcf7 ) {
-   global $wpdb;
+	global $wpdb;
 
 	$submission = WPCF7_Submission::get_instance();
 
-	if(!empty($submission)){
+	if ( ! empty( $submission ) ) {
 		$wpcf7->title();
 		$form_data = $submission->get_posted_data();
-		if($form_data['_wpcf7'] != 0){
+		if ( $wpcf7->id !== 0 ) {
 			$cf7Id = array();
-	
-			if(get_option('cf7db')!=''){
-				$cf7Id = json_decode(get_option('cf7db'), true);
+
+			if ( get_option( 'cf7db' ) != '' ) {
+				$cf7Id = json_decode( get_option( 'cf7db' ), true );
 			}
-			
-			$form_id = $form_data['_wpcf7'];
-			if(in_array($form_id,$cf7Id)){
-				foreach($form_data as $key=>$value){
-					if(preg_match('/^_/', $key)){
-						unset($form_data[$key]);
+
+			$form_id = $wpcf7->id;
+			if ( in_array( $form_id, $cf7Id ) ) {
+				foreach ( $form_data as $key => $value ) {
+					if ( preg_match( '/^_/', $key ) ) {
+						unset( $form_data[ $key ] );
 					}
 				}
-				$form_values = json_encode($form_data);
-				$table_name = $wpdb->prefix . "cf7_export_csv_db"; 
-				$wpdb->insert($table_name, array('form_id' => $form_id, 'form_values' => $form_values));
+				$form_values = json_encode( $form_data );
+				$table_name  = $wpdb->prefix . "cf7_export_csv_db";
+				$wpdb->insert( $table_name, array( 'form_id' => $form_id, 'form_values' => $form_values ) );
 			}
 		}
 	}
-   
+
 }
